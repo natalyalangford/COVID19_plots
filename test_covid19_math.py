330eef2ebd2bc22bcce77fca6e6574342baa1750
Natalya <natalyaclangford@gmail.com>
2020-04-26 23:33:11 -0700

    added test functions

--- test_covid19_math.py
+++ test_covid19_math.py
@@ -4,7 +4,6 @@ import unittest
 import numpy as np
 import covid19_math as cvm
 
-
 class Test_0_series_rolling_doubling_time(unittest.TestCase):
 
     def setUp(self) -> None:
@@ -15,11 +14,42 @@ class Test_0_series_rolling_doubling_time(unittest.TestCase):
                                      'response': [np.nan, 7.78, 7.2, 6.15, 7.68, 4.08, 3.66, 3.34, 2.85, 4.34,
                                                   4.51, 4.91, 4.57, 4.24, 4.19, 4.72, 5.44, 6.17, 5.72, 5.21,
                                                   4.56, 4.41, 5.36]
-                                     }}
+                                     },
+                           'test2': {'expr': cvm.CovidMath.series_rolling_doubling_time(
+                                                 [200.0, 239.0, 267.0, 314.0, 314.0, 559.0, 689.0, 886.0, 1058.0,
+                                                  1243.0], 5),
+                                     'response':[np.nan, 7.78, 7.2, 6.15, 7.68, 4.08, 3.66, 3.34, 2.85, 4.34]
+                                     },
+                           'test3': {'expr': cvm.CovidMath.series_rolling_doubling_time(
+                                                 [200, 239, 267, 314, 314, 559, 689, 886], 5),
+                                     'response': [np.nan, 7.78, 7.2, 6.15, 7.68, 4.08, 3.66, 3.34]
+                                     },
+                           'test4': {'expr': cvm.CovidMath.series_rolling_doubling_time(
+                                                 [np.nan, 239, 267, 314, 314, 559, 689, 886, 1058, 1243, 1486, 1795,
+                                                  2257, 2815, 3401, 3743, 4269, 4937, 6235, 7284, 9134, 10836, 11899], 5
+                                                  ),
+                                     'response': [np.nan, np.nan, 12.51, 7.62, 10.16, 4.08, 3.66, 3.34, 2.85, 4.34, 4.51,
+                                                  4.91, 4.57, 4.24, 4.19, 4.72, 5.44, 6.17, 5.72, 5.21, 4.56, 4.41, 5.36
+                                                  ]
+                                     },
+                           'test5': {'expr': cvm.CovidMath.series_rolling_doubling_time(
+                                                  [239, 267], 5),
+                                     'response': [np.nan, 12.51]
+                                     },
+                           'test6': {'expr': cvm.CovidMath.series_rolling_doubling_time(
+                                                  [239], 5),
+                                     'response': [np.nan]
+                                     },
+                           'test7': {'expr': cvm.CovidMath.series_rolling_doubling_time(
+                                                  [], 5),
+                                     'response': [np.nan]
+
+        }
 
     def test_1(self):
         for test_name, test_case in self.test_cases.items():
             self.assertEqual(test_case['expr'], test_case['response'])
+
 
 
 if __name__ == "__main__":
